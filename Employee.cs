9e9f8f789e02edb520f1e72125706a8ae792f28b
Rahul Mondal <rahul.arambagh96@gmail.com>
2019-02-07 14:40:42 +0530

    files are added

---
+++ Employee.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Employee
+    {
+        public int Id;
+        public string Name;
+        public string DepartmentName;
+        public event CallMethodDelegate CallMethod;
+        //public event CallMethodDelegate CallMethod;
+        //public CallMethodEventArgs args = new CallMethodEventArgs();
+        public Employee()
+        {
+            Console.WriteLine("Enter Your Id:");
+            this.Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Name:");
+            this.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Department:");
+            this.DepartmentName = Console.ReadLine();
+        }
+
+        public void GetId() {
+
+            /*args.methodName = "GetId()";
+            CallMethod(this, args);*/
+            CallMethod("GetId()");
+            Console.WriteLine("Employee Id: " +Id);
+            Console.WriteLine();
+        }
+
+        public void GetName()
+        {
+            /*args.methodName = "GetName()";
+            CallMethod(this, args);*/
+            CallMethod("GetName()");
+            Console.WriteLine("Employee Name: " +Name);
+            Console.WriteLine();
+        }
+
+        public void GetDepartmentName()
+        {
+            /*args.methodName = "GetDepartmentName()";
+             CallMethod(this, args);*/
+            CallMethod("GetDepartmentNmae()");
+            Console.WriteLine("Employee Department Name: " +DepartmentName);
+            Console.WriteLine();
+        }
+        public string EditDetails(int id)
+        {
+            Console.WriteLine("Update Your Id:");
+            string updatedId= Console.ReadLine();
+            return updatedId;
+        }
+
+        public string EditDetails()
+        {
+            Console.WriteLine("Update Your Name:");
+            string updatedName = Console.ReadLine();
+            return updatedName;
+        }
+
+
+        public string EditDetails(string Department)
+        {
+            Console.WriteLine("Update Your Department Name:");
+            string updatedDepartment = Console.ReadLine();
+            return updatedDepartment;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cerating object of Employee class
+            Employee employee = new Employee();
+
+            //employee.CallMethod += new CallMethodDelegate(onCallMethod);
+
+            employee.CallMethod += new CallMethodDelegate(onCallMethod);
+            
+            //calling the method of Employee class
+            employee.GetId();
+            employee.GetName();
+            employee.GetDepartmentName();
+           
+            int updatedId = int.Parse(employee.EditDetails(employee.Id));
+            string updatedName = employee.EditDetails();
+            string updatedDepatment = employee.EditDetails(employee.DepartmentName);
+
+            //printing all the value of employee details
+           
+            Console.WriteLine("updated name:" + updatedId);
+            Console.WriteLine("updated name:" + updatedName);
+            Console.WriteLine("updated department:" + updatedDepatment);
+
+
+            Console.ReadKey();
+        }
+
+        static void onCallMethod(string methodname)
+        {
+            Console.WriteLine($"{methodname} method called");
+        }
+        /*static void onCallMethod(object sender,CallMethodEventArgs args)
+        {
+            Console.WriteLine($"{args.methodName} method called");
+        }*/
+    }
+}
